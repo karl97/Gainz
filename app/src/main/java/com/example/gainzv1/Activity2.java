85f6c86f2e45feba144a5b4d61b199020d465d4c
Karl Eriksson <karl.97@live.se>
2021-10-03 19:18:01 +0200

    3 Pages. Added 2 activites, so now there are a total of 3 pages. Home, Train and Journey pages.

--- app/src/main/java/com/example/gainzv1/Activity2.java
+++ app/src/main/java/com/example/gainzv1/Activity2.java
@@ -10,23 +10,20 @@ import android.view.WindowInsetsController;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity {
 
     Intent intent;
     ArrayList<ImageButton> bottomnavbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getWindow().setDecorFitsSystemWindows(false);
+        setContentView(R.layout.activity_2);
         WindowInsetsController controller = getWindow().getInsetsController();
         if (controller != null) {
             controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
             controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
         }
-
         bottomnavbar = new ArrayList<ImageButton>();
         bottomnavbar.add((ImageButton) findViewById(R.id.bottom_toolbar_btn1));
         bottomnavbar.add( (ImageButton) findViewById(R.id.bottom_toolbar_btn2));
@@ -35,25 +32,26 @@ public class MainActivity extends AppCompatActivity {
 
 
         bottomnavbar.forEach((btn)-> btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switch (view.getId())
-                        {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId())
+                {
 
-                            case R.id.bottom_toolbar_btn1:
-                                break;
-                            case R.id.bottom_toolbar_btn2:
-                                intent = new Intent(MainActivity.this, Activity2.class);
-                                startActivity(intent);
-                                break;
-                            case R.id.bottom_toolbar_btn3:
-                                intent = new Intent(MainActivity.this, Activity3.class);
-                                startActivity(intent);
-                                break;
-                            default:
-                        }
+                    case R.id.bottom_toolbar_btn1:
+                        intent = new Intent(Activity2.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.bottom_toolbar_btn2:
 
-                    }
-                }));
+                        break;
+                    case R.id.bottom_toolbar_btn3:
+                        intent = new Intent(Activity2.this, Activity3.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                }
+
+            }
+        }));
     }
 }
